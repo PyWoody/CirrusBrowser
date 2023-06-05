63059c14b4a61328cca897c1e98cfc87b0a04a8d
Chromebook <sam@woodward.fyi>
2023-06-04 20:46:31 -0400

    Improving long_running_action decorator.

--- cirrus/utils/__init__.py
+++ cirrus/utils/__init__.py
@@ -56,44 +56,38 @@ def execute_ss_callback(func):
 
 class long_running_action:
 
+    timer_events = dict()
 
     def __init__(self, wait=100, cursor=Qt.WaitCursor):
         self.wait = wait
         self.cursor = cursor
-        self.timer_events_status = dict()
-        self.orig_timer_event = None
         self.cls = None
 
     def __call__(self, func):
         def cb(*args, **kwargs):
             self.cls = args[0]
-            if self.orig_timer_event is None:
-                self.orig_timer_event = self.cls.timerEvent
+            orig_timer_event = self.cls.timerEvent
             self.cls.timerEvent = self.timerEvent
             timer_id = self.cls.startTimer(self.wait)
-            self.timer_events_status[timer_id] = False
+            self.timer_events[timer_id] = (orig_timer_event, False)
             result = func(*args, **kwargs)
-            QTimer.singleShot(0, partial(self.finished, timer_id))
+            QTimer.singleShot(0, partial(self.finished, orig_timer_event, timer_id))
             return result
         return cb
 
-    def finished(self, timer_id):
-        self.timer_events_status[timer_id] = True
-
-    def restore(self):
-        if not self.timer_events_status:
-            self.cls.timerEvent = self.orig_timer_event
+    def finished(self, orig_timer, timer_id):
+        self.timer_events[timer_id] = (orig_timer, True)
 
     def timerEvent(self, event):
         timer_id = event.timerId()
-        completed = self.timer_events_status.get(timer_id)
-        if completed is not None:
+        if running_event := self.timer_events.get(timer_id):
+            orig_timer, completed = running_event
             if completed:
                 if self.cls.cursor().shape() != Qt.ArrowCursor:
                     self.cls.setCursor(Qt.ArrowCursor)
                 self.cls.killTimer(timer_id)
-                del self.timer_events_status[timer_id]
-                self.restore()
+                self.cls.timerEvent = orig_timer
+                del self.timer_events[timer_id]
             else:
                 if self.cls.cursor().shape() != self.cursor:
                     self.cls.setCursor(self.cursor)
