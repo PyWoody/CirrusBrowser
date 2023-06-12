4d528b742812e838c83d87467379e431ad51809e
Chromebook <sam@woodward.fyi>
2023-06-12 09:14:43 -0400

    Add is_main_thread decorator

--- cirrus/utils/threads.py
+++ cirrus/utils/threads.py
@@ -1,5 +1,15 @@
+from functools import wraps
 import threading
 
 
 def assert_is_main_thread():
     assert threading.current_thread() is threading.main_thread()
+
+
+def decorator_assert_is_main_thread(func):
+    @wraps(func)
+    def cb(*args, **kwargs):
+        assert_is_main_thread()
+        result = func(*args, **kwargs)
+        return result
+    return cb
