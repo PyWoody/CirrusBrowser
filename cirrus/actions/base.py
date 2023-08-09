a84ddb600ecabec588b85772d295e4387dd18001
Chromebook <sam@woodward.fyi>
2023-08-09 17:00:47 -0400

    Adding conflict resolution directly into the BaseAction for convenience

--- cirrus/actions/base.py
+++ cirrus/actions/base.py
@@ -1,5 +1,5 @@
 from PySide6.QtGui import QAction
-from PySide6.QtCore import QRunnable, Signal
+from PySide6.QtCore import QRunnable, Slot, Signal
 
 
 class BaseRunnable(QRunnable):
@@ -12,6 +12,14 @@ class BaseRunnable(QRunnable):
 
 class BaseAction(QAction):
     accepted = Signal()
+
+    @Slot(str, bool)
+    def update_conflict(self, conflict, checked):
+        """Updates the Action's conflict value to the specified `conflict`
+        Useful for connecting to the Dialog's .toggled signal
+        """
+        if checked:
+            self.conflict = conflict
 
     def show_dialog(self, *args, **kwargs):
         return False
